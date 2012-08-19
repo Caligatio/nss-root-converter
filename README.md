3f3f38544a034548438cbec30fe3382d8c68b34c
Brian Turek <brian.turek@gmail.com>
2012-08-19 15:05:22 -0400

    Added Python 3.2 script and updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-nss-root-converter
-==================
+Extracting Mozilla's NSS Root Certificates
+==========================================
 
-Converts the NSS certificate roots to a more-friendly PEM formatted file
+With Python 3.2 now natively supporting SSL certificate validation, a Pythonic way is needed to create a PEM formatted certificate chain file.  While many people use the file made available by the cURL project (http://curl.haxx.se/ca/cacert.pem), it is not served via HTTPS and is therefore insecure to download.  Fortunately, the source of the cURL's certificate authority bundle is available over HTTPS but unfortunately is in a difficult-to-use format:
+
+    https://mxr.mozilla.org/mozilla/source/security/nss/lib/ckfw/builtins/certdata.txt?raw=1
+
+These Python scripts will download the Mozilla NSS-friendly file (unless the script itself is given a PEM-formatted CA file, the download will be unvalidated) and convert it into the PEM format which then can be used by Python, cURL, php_curl, Apache/mod_ssl, etc.
+
+Note that this functionality is available already in a few other languages:
+	Go - https://github.com/agl/extract-nss-root-certs
+	Perl - https://github.com/bagder/curl/blob/master/lib/mk-ca-bundle.pl
---
+++ nss-coverter-py32.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+'''
+Copyright (c) 2012, Brian Turek
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * The names of the contributors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ANDANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIEDWARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT,INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING,BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE,DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OFLIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCEOR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISEDOF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+from gzip import GzipFile
+from io import BytesIO
+from base64 import b64encode
+from textwrap import wrap
+from urllib.request import urlopen, Request
+from argparse import ArgumentParser
+import sys
+import ssl
+import re
+
+
+def fetchUrl(url, caFile = None):
+	'''
+	fetchUrl(url[, caFile = None]) -> bytes
+
+	Gets the contents of a URL, performing GZip decoding if needed
+	'''
+	# Setup headers to be used with accessing Southwest's website
+	headers = {'Accept-Encoding': 'gzip'}
+
+	request = Request(url, None, headers)
+	sock = urlopen(request, cafile = caFile)
+	data = sock.read()
+
+	# Check for GZip encoding and decode if needed
+	if (sock.headers.get('content-encoding', None) == 'gzip'):
+		data = GzipFile(fileobj=BytesIO(data)).read()
+
+	return data
+
+def parseNSSFile(caFile = None, explicitTrustOnly = True, trustServerAuth = True,
+	trustEmailProtection = False, trustCodeSigning = False):
+	'''
+	parseNSSFile(caFile = None, explicitTrustOnly = True, trustServerAuth = True,
+		trustEmailProtection = False, trustCodeSigning = False) -> tuple
+
+	Downloads and parses out the license, date, and trusted certificate roots
+	contained in the NSS certificate root file.
+	'''
+	objs = []
+	currObj = None
+	license = None
+	date = None
+
+	if (None == caFile):
+		sys.stdout.write("Warning: accessing the NSS certificate root file without SSL validation")
+
+	try:
+		raw = fetchUrl('https://mxr.mozilla.org/mozilla/source/security/nss/lib/ckfw/builtins/certdata.txt?raw=1',
+			caFile)
+	except (ssl.SSLError) as e:
+		sys.stderr.write("Error: cannot find needed SSL certificate in CAFile, aborting\n")
+		return None
+
+	content = raw.decode('utf-8')
+
+	lines = content.splitlines()
+
+	for (i, line) in enumerate(lines):
+		if (-1 != line.find('This Source Code Form')):
+			license = getLicense(lines, i)
+
+		if (line.startswith('CVS_ID')):
+			date = re.findall('\$Date: (.*) \$', line)[0]
+
+		if (not line.startswith('CKA')):
+			continue
+
+		words = line.split(' ', 2)
+
+		if ("CKA_CLASS" == words[0]):
+			if (None != currObj):
+				objs.append(currObj)
+
+			currObj = {}
+
+		if ('MULTILINE_OCTAL' == words[1]):
+			value = parseMultlineOctal(lines, i + 1)
+		else:
+			value = words[2]
+
+		currObj[words[0]] = {'type' : words[1], 'value' : value}
+
+	certs = (x for x in objs if x['CKA_CLASS']['value'] == 'CKO_CERTIFICATE')
+	trusts = [x for x in objs if x['CKA_CLASS']['value'] == 'CKO_NSS_TRUST']
+
+	trustedCerts = []
+	for cert in certs:
+		if	(isCertTrusted(cert, trusts, explicitTrustOnly, trustServerAuth,
+				trustEmailProtection, trustCodeSigning)):
+			trustedCerts.append(cert)
+
+	return (license, date, trustedCerts)
+
+def parseMultlineOctal(lines, num):
+	'''
+	parseMultlineOctal(lines, num) -> list(number)
+
+	Parses out a MULTILINE_OCTAL block and returns a list of the byte contents
+	'''
+	value = []
+
+	while True:
+		if ('END' == lines[num]):
+			# Convert octal digits into ints using map/lambda
+			return bytes(map(lambda x: int(x, 8), value))
+		elif (lines[num].startswith('\\')):
+			# Skip the first item as its empty
+			value.extend(lines[num].split('\\')[1:])
+		else:
+			return None
+		num += 1
+
+def getLicense(lines, num):
+	'''
+	getLicense(lines, num) -> string
+
+	Parses out the license and returns it as a single string
+	'''
+	license = []
+
+	while True:
+		if (lines[num].startswith('CVS_ID')):
+			break
+		license.append(lines[num][2:])
+
+		num += 1
+
+	return ' '.join(license)
+
+def isCertTrusted(cert, trusts, explicitTrustOnly, trustServerAuth,
+		trustEmailProtection, trustCodeSigning):
+	'''
+	isCertTrusted(cert, trusts, explicitTrustOnly, trustServerAuth,
+		trustEmailProtection, trustCodeSigning) -> boolean
+
+	Examines a certificate and evaluates it against the trust objects and
+	criteria to determine if it should be trusted
+	'''
+	issuer = cert['CKA_ISSUER']['value']
+	serial = cert['CKA_SERIAL_NUMBER']['value']
+
+	for trust in trusts:
+		if ((trust['CKA_ISSUER']['value'] == issuer) and (trust['CKA_SERIAL_NUMBER']['value'] == serial)):
+			if (trustServerAuth):
+				if (	(trust['CKA_TRUST_SERVER_AUTH']['value'] == 'CKT_NSS_TRUSTED_DELEGATOR')
+						or (	(trust['CKA_TRUST_SERVER_AUTH']['value'] == 'CKT_NSS_MUST_VERIFY_TRUST')
+							and (not explicitTrustOnly)
+						)
+					):
+						return True
+
+			if (trustEmailProtection):
+				if (	(trust['CKA_TRUST_EMAIL_PROTECTION']['value'] == 'CKT_NSS_TRUSTED_DELEGATOR')
+						or (	(trust['CKA_TRUST_EMAIL_PROTECTION']['value'] == 'CKT_NSS_MUST_VERIFY_TRUST')
+							and (not explicitTrustOnly)
+						)
+					):
+						return True
+
+			if (trustCodeSigning):
+				if (	(trust['CKA_TRUST_CODE_SIGNING']['value'] == 'CKT_NSS_TRUSTED_DELEGATOR')
+					or (	(trust['CKA_TRUST_CODE_SIGNING']['value'] == 'CKT_NSS_MUST_VERIFY_TRUST')
+						and (not explicitTrustOnly)
+					)
+				):
+					return True
+
+	else:
+		return False
+
+def main(outFile, caFile = None, explicitTrustOnly = True, trustServerAuth = True,
+	trustEmailProtection = False, trustCodeSigning = False):
+	'''
+	main(outFile, caFile = None, explicitTrustOnly = True, trustServerAuth = True,
+		trustEmailProtection = False, trustCodeSigning = False)
+
+	Downloads and parses the NSS certificate root file, writing out the license,
+	data, and trusted certificates to a PEM file
+	'''
+	parsedNSS = parseNSSFile(caFile,
+					explicitTrustOnly,
+					trustServerAuth,
+					trustEmailProtection,
+					trustCodeSigning)
+
+	if (None == parsedNSS):
+		sys.stderr.write("Could not find any certificates\n")
+		return
+
+	try:
+		f = open(outFile, 'wt', encoding='utf-8')
+	except (IOError) as e:
+		sys.stderr.write("Error: could not open output file\n")
+		return
+
+	f.write("\n".join(
+		wrap(
+			parsedNSS[0],
+			76,
+			initial_indent = '## ',
+			subsequent_indent = '## '
+		)
+	) + "\n")
+
+	f.write("\n## Date: " + parsedNSS[1] + "\n\n")
+
+	for trustedCert in parsedNSS[2]:
+		name = trustedCert['CKA_LABEL']['value'].strip("\"")
+
+		f.write(name + "\n")
+		f.write("=" * len(name) + "\n")
+		f.write("-----BEGIN CERTIFICATE-----\n")
+		f.write("\n".join(
+			wrap(
+				b64encode(trustedCert['CKA_VALUE']['value']).decode('utf-8'),
+				76)
+			) + "\n")
+		f.write("-----END CERTIFICATE-----\n\n")
+
+	f.close()
+
+if (__name__ == "__main__"):
+	parser = ArgumentParser(
+		description = 'Downloads the Mozilla NSS root certificates and coverts to PEM format')
+	parser.add_argument('outFile', help = 'Output filename for PEM certificates')
+	parser.add_argument('--caFile', dest = 'caFile', metavar = 'FILE',
+		help = 'CAChain file to be used to obtain the NSS root certificates')
+	parser.add_argument('--trustExplictOnly', action = 'store_true',
+		default = True)
+	parser.add_argument('--trustServerAuth', action = 'store_true',
+		default = True)
+	parser.add_argument('--trustEmailProtection', action = 'store_true',
+		default = False)
+	parser.add_argument('--trustCodeSigning', action = 'store_true',
+		default = False)
+
+	args = parser.parse_args()
+
+	main(args.outFile, args.caFile, args.trustExplictOnly, args.trustServerAuth,
+		args.trustEmailProtection, args.trustCodeSigning)
